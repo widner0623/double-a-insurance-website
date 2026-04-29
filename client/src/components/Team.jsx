0a0be74e0717f3cd10e16f706886d3e3eeebb662
Derrick Widner <derrick.widner@outlook.com>
2026-04-29 13:00:50 -0500

    test

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -45,7 +45,7 @@ function Team() {
               <img
                 src={agent.image}
                 alt={agent.name}
-                className="h-[500px] mt-16 w-full object-cover"
+                className="h-[500px] pt-16 w-full object-cover"
               />
 
               <div className="p-8">
